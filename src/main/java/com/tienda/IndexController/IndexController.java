0de6fae93e66c9db4225c8b112218d3664fdf96d
FaceBasu <axelpsn27@gmail.com>
2022-10-08 18:31:31 -0600

    Esta es la tarea de la semana 4 mysql

--- src/main/java/com/tienda/IndexController/IndexController.java
+++ src/main/java/com/tienda/IndexController/IndexController.java
@@ -1,7 +1,11 @@
-package com.tienda.controller;
+package com.tienda.IndexController;
 
+
+
+import com.tienda.dao.ClienteDao;
 import com.tienda.dominio.Cliente;
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +14,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     
     @Autowired
-    private CleinteDao clienteDao;
+    private ClienteDao clienteDao;
     
     @GetMapping("/")
     public String inicio(Model model) {
         var texto="Estamos en semana 4";
         model.addAttribute("saludo", texto);
         
-        Cliente cliente1 = new Cliente("Pedro", "Jimenez Retana", "pjimenez@gmail.com", "88888888");
+        /*Cliente cliente1 = new Cliente("Pedro", "Jimenez Retana", "pjimenez@gmail.com", "88888888");
         Cliente cliente2 = new Cliente("Juan", "Retana Jimenez", "pretana@gmail.com", "88888887");
         Cliente cliente3 = new Cliente("Pablo", "Jerez Rodriguez", "pjerez@gmail.com", "88888886");
-        var clientes=Arrays.asList(cliente1, cliente2, cliente3);
+        var clientes=Arrays.asList(cliente1, cliente2, cliente3);*/
         
-        
-        model.addAttribute("clientes", clientes);
+        var clientes = clienteDao.findAll();
+        model.addAttribute("clientes",clientes);
         
         return "index";
     }
